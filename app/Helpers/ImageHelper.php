90f1b3cae6e1102ceb35a95e44605e6eb69abba4
AmadulHaque <92516695+AmadulHaque@users.noreply.github.com>
2024-10-30 18:23:33 +0600

    update image service

--- app/Helpers/ImageHelper.php
+++ app/Helpers/ImageHelper.php
@@ -3,98 +3,135 @@
 namespace App\Helpers;
 
 use Illuminate\Support\Str;
-use Image;
+use Intervention\Image\Facades\Image;
 
 class ImageHelper
 {
-    public static function handleUploadedImage($file,$path,$delete=null) {
+    public static function handleUploadedImage($file, $path, $delete = null) {
         if ($file) {
-            if($delete){
-                
-                if (file_exists(base_path('../').$path.'/'.$delete)) {
-                    unlink(base_path('../').$path.'/'.$delete);
-                }
+            $fullPath = public_path($path);
+
+            // Delete existing file if specified
+            if ($delete && file_exists($fullPath . '/' . $delete)) {
+                unlink($fullPath . '/' . $delete);
             }
-            $name = Str::random(4).$file->getClientOriginalName();
-            $file->move($path,$name);
+
+            // Generate unique name and move file
+            $name = Str::random(4) . '_' . time() . '_' . $file->getClientOriginalName();
+            if (!file_exists($fullPath)) {
+                mkdir($fullPath, 0777, true);
+            }
+            $file->move($fullPath, $name);
+
             return $name;
         }
+        return null;
     }
 
-
-    public static function uploadSummernoteImage($file,$path) {
+    public static function uploadSummernoteImage($file, $path) {
         if ($file) {
-            $name = Str::random(4).$file->getClientOriginalName();
-            $file->move($path,$name);
+            $fullPath = public_path($path);
+            $name = Str::random(4) . '_' . time() . '_' . $file->getClientOriginalName();
+
+            if (!file_exists($fullPath)) {
+                mkdir($fullPath, 0777, true);
+            }
+
+            $file->move($fullPath, $name);
             return $name;
         }
+        return null;
     }
-
-
-
-    public static function ItemhandleUploadedImage($file,$path,$delete=null) {
-        if ($file) {
-            if($delete){
-                if (file_exists(base_path('../').$path.'/'.$delete)) {
-                    unlink(base_path('../').$path.'/'.$delete);
-                }
-            }
-
-            $thum = Str::random(8).'.'.$file->getClientOriginalExtension();
-            $image = \Image::make($file)->resize(230,230);
+    public static function handleUpdatedUploadedImage($file, $path, $data, $delete_path, $field) {
+        // Generate unique file name
+        $name = time() . '_' . $file->getClientOriginalName();
     
-            $image->save(base_path('../').$path.'/'.$thum);
+        // Resolve the full path to the public directory
+        $fullPath = public_path($path);
     
-            $photo = time().$file->getClientOriginalName();
-            $file->move($path,$photo);
-            return [$photo,$thum];
+        // Ensure the directory exists, create if not
+        if (!file_exists($fullPath)) {
+            mkdir($fullPath, 0777, true);
         }
-    }
-
-    public static function handleUpdatedUploadedImage($file,$path,$data,$delete_path,$field) {
-        $name = time().$file->getClientOriginalName();
-   
-        $file->move(base_path('..').$path,$name);
-        if($data[$field] != null){
-            if (file_exists(base_path('../').$delete_path.$data[$field])) {
-                unlink(base_path('../').$delete_path.$data[$field]);
+    
+        // Move the uploaded file to the target directory
+        $file->move($fullPath, $name);
+    
+        // Delete the old file if it exists
+        if (!empty($data[$field])) {
+            $oldFile = public_path($delete_path . '/' . $data[$field]);
+            if (file_exists($oldFile)) {
+                unlink($oldFile);
             }
         }
+    
         return $name;
     }
+    
 
+    
+    public static function ItemhandleUploadedImage($file, $path, $delete = null) {
+        if ($file) {
+            $fullPath = public_path($path);
 
-    public static function ItemhandleUpdatedUploadedImage($file,$path,$data,$delete_path,$field) {
-        $photo = time().$file->getClientOriginalName();
-        $thum = Str::random(8).'.'.$file->getClientOriginalExtension();
-      
-        $image = \Image::make($file)->resize(230,230);
-
-        $image->save(base_path('..').$path.'/'.$thum);
-
-        $file->move(base_path('..').$path,$photo);
-
-        if($data['thumbnail'] != null){
-            if (file_exists(base_path('../').$delete_path.$data['thumbnail'])) {
-                unlink(base_path('../').$delete_path.$data['thumbnail']);
+            // Delete existing file if specified
+            if ($delete && file_exists($fullPath . '/' . $delete)) {
+                unlink($fullPath . '/' . $delete);
             }
-        }
-        if($data[$field] != null){
-            if (file_exists(base_path('../').$delete_path.$data[$field])) {
-                unlink(base_path('../').$delete_path.$data[$field]);
+
+            // Handle thumbnail generation and main photo upload
+            $thumbnailName = Str::random(8) . '.' . $file->getClientOriginalExtension();
+            $photoName = time() . '_' . $file->getClientOriginalName();
+
+            // Ensure directory exists
+            if (!file_exists($fullPath)) {
+                mkdir($fullPath, 0777, true);
             }
+
+            // Save resized thumbnail and move main photo
+            $image = Image::make($file)->resize(230, 230);
+            $image->save($fullPath . '/' . $thumbnailName);
+            $file->move($fullPath, $photoName);
+
+            return [$photoName, $thumbnailName];
         }
-        return [$photo,$thum];
+        return [null, null];
     }
 
+    public static function ItemhandleUpdatedUploadedImage($file, $path, $data, $deletePath, $field) {
+        $fullPath = public_path($path);
+        $deleteFullPath = public_path($deletePath);
 
-    public static function handleDeletedImage($data,$field,$delete_path) {
-        
-        
-        if($data[$field] != null){
-            if (file_exists(base_path('../').$delete_path.$data[$field])) {
-                unlink(base_path('../').$delete_path.$data[$field]);
-            }
+        // Generate new file names
+        $photoName = time() . '_' . $file->getClientOriginalName();
+        $thumbnailName = Str::random(8) . '.' . $file->getClientOriginalExtension();
+
+        // Ensure directory exists
+        if (!file_exists($fullPath)) {
+            mkdir($fullPath, 0777, true);
+        }
+
+        // Save resized thumbnail and move main photo
+        $image = Image::make($file)->resize(230, 230);
+        $image->save($fullPath . '/' . $thumbnailName);
+        $file->move($fullPath, $photoName);
+
+        // Delete existing thumbnail and main photo if they exist
+        if (!empty($data['thumbnail']) && file_exists($deleteFullPath . '/' . $data['thumbnail'])) {
+            unlink($deleteFullPath . '/' . $data['thumbnail']);
+        }
+        if (!empty($data[$field]) && file_exists($deleteFullPath . '/' . $data[$field])) {
+            unlink($deleteFullPath . '/' . $data[$field]);
+        }
+
+        return [$photoName, $thumbnailName];
+    }
+
+    public static function handleDeletedImage($data, $field, $deletePath) {
+        $deleteFullPath = public_path($deletePath);
+
+        if (!empty($data[$field]) && file_exists($deleteFullPath . '/' . $data[$field])) {
+            unlink($deleteFullPath . '/' . $data[$field]);
         }
     }
 }
